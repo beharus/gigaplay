39013b62468ccce7fa375a662abe2314d17a1358
iamaustin316 <iamaustin316@gmail.com>
2024-01-10 10:31:13 +0800

    fix

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,28 +1,47 @@
-import { motion } from "framer-motion"
-import { variant2 } from "../utils/motion"
-import { featuredData } from "../assets/dummy"
-import Button from "./button"
+import { motion } from "framer-motion";
+import { variant2 } from "../utils/motion";
+import { featuredData } from "../assets/dummy";
+import Button from "./button";
+import React from "react";
 
 const Features = () => {
   return (
     <div className=" min-h-screen p-5 lg:sticky top-28 flex justify-center md:justify-end max-w-[1440px] w-full mx-auto">
       <div className=" md:max-w-md w-full">
-        <motion.h1 variants={variant2(0.5)}
+        <motion.h1
+          variants={variant2(0.5)}
           initial="hidden"
           whileInView="show"
-          className=" text-teal-200 font-medium text-4xl my-5">{featuredData.h1}</motion.h1>
-        <motion.div variants={variant2(0.7)}
+          className=" text-teal-200 font-medium text-4xl my-5"
+        >
+          {featuredData.h1}
+        </motion.h1>
+        <motion.div
+          variants={variant2(0.7)}
           initial="hidden"
           whileInView="show"
-          className=" bg-slate-900/50 md:bg-slate-700/50 border-2 border-slate-700 p-8 rounded-xl">
-          <video className=" rounded-lg" playsInline loop muted autoPlay preload='true' src={featuredData.video_url}></video>
-          <h1 className="text-4xl text-white font-semibold mt-6">{featuredData.card_h1}</h1>
-          <p className=" text-lg my-5 text-white opacity-40">{featuredData.card_p}</p>
-          <Button text={'Play Now'}/>
+          className=" bg-slate-900/50 md:bg-slate-700/50 border-2 border-slate-700 p-8 rounded-xl"
+        >
+          <video
+            className=" rounded-lg"
+            playsInline
+            loop
+            muted
+            autoPlay
+            preload="true"
+            src={featuredData.video_url}
+          ></video>
+          <h1 className="text-4xl text-white font-semibold mt-6">
+            {featuredData.card_h1}
+          </h1>
+          <p className=" text-lg my-5 text-white opacity-40">
+            {featuredData.card_p}
+          </p>
+          <Button text={"Play Now"} />
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,19 +1,34 @@
-import { gamesData } from "../assets/dummy"
-import GameCard from "./gameCard"
+import { gamesData } from "../assets/dummy";
+import GameCard from "./gameCard";
+
+import React from "react";
 
 const Games = () => {
   return (
-    <>
-      <h1 className=' py-9 text-center text-teal-200 text-4xl'>{gamesData.h1}</h1>
+    <React.Fragment>
+      <h1 className=" py-9 text-center text-teal-200 text-4xl">
+        {gamesData.h1}
+      </h1>
       <div className="flex justify-center md:justify-between gap-10 flex-wrap md:flex-nowrap">
-        {
-          gamesData.cards.map(({ delay, img, bg_img, card_h1, card_type, card_p, button }, key) => {
-            return <GameCard key={key} delay={delay} img={img} bg_img={bg_img} card_h1={card_h1} card_type={card_type} card_p={card_p} button={button} />
-          })
-        }
+        {gamesData.cards.map(
+          ({ delay, img, bg_img, card_h1, card_type, card_p, button }, key) => {
+            return (
+              <GameCard
+                key={key}
+                delay={delay}
+                img={img}
+                bg_img={bg_img}
+                card_h1={card_h1}
+                card_type={card_type}
+                card_p={card_p}
+                button={button}
+              />
+            );
+          }
+        )}
       </div>
-    </>
-  )
-}
+    </React.Fragment>
+  );
+};
 
-export default Games
+export default Games;
